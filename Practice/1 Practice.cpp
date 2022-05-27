5833955fd6b3048da989e75170668c61cea55934
mhshitol <105976383+mhshitol@users.noreply.github.com>
2022-05-27 16:01:56 +0600

    Add files via upload

---
+++ Practice/1 Practice.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("HELLO EVERYONE...\n");
+    printf("I am MINHAJUL ISLAM SHITOL.");
+
+
+
+    return 0;
+}
---
+++ Practice/2 practice.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    printf("number : %d",a);
+
+
+    return 0;
+
+
+}
