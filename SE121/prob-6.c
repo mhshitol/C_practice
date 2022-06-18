7bf755b79b69e2f541683fdabd1f25a0c7cc7d06
Minhajul Islam Shitol <mhshitol27@gmail.com>
2022-06-19 01:41:24 +0600

    se121

--- SE121/prob-6.c
+++ SE121/prob-6.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int num, sum=0;
+    int num,r,sum=0;
 
     printf("Enter any number to find sum of its digit: ");
     scanf("%d", &num);
 
     while(num!=0)
     {
-        sum += num % 10;
+        r=num%10;
+        sum=sum+r;
         num = num / 10;
     }
     printf("Sum of digits = %d", sum);
