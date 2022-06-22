d0e0e582abfb6c1cd27ffc0ddf398ff503ed17b6
Minhajul Islam Shitol <mhshitol27@gmail.com>
2022-06-22 20:42:46 +0600

    se121

--- SE121/prob-7.c
+++ SE121/prob-7.c
@@ -7,7 +7,8 @@ int main() {
   printf("Enter the integer: ");
   scanf("%d", &n);
 
-  while (n != 0) {
+  while (n != 0)
+    {
     remainder = n % 10;
     reverse = reverse * 10 + remainder;
     n=n/10;
